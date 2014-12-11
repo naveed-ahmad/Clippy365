715480ec15e17ecd6ebaf2a9bae043f71df44a05
Vel0x <DaleMyers19@gmail.com>
2014-12-11 11:49:40 -0800

    remove yes/no from person

--- clippyHelpers.js
+++ clippyHelpers.js
@@ -349,67 +349,38 @@ function similarImages(data){
 
 function wikipedia(data){
 	this.clippyInAction = true;
+	var text = documentData;
 	if(Object.keys(data).length == 0){
-		agent.speak("Would you like me to try and find some Wikipedia articles?");
-		addClippyOptions([
-							{
-								"name":"yes",
-								"text":"Yes",
-								"data":{"type":"result","result":"yes"},
-								"callback":wikipedia
-							},
-							{
-								"name":"no",
-								"text":"No",
-								"data":{"type":"result","result":"no"},
-								"callback":wikipedia
-							}
-						]);
-		return;
-	}
-	
-	//We were the caller so we are doing letter specific stuff
-	if(data["type"] == "result"){
-		if(data["result"] == "yes"){
-			log('Got a yes result');
-			var text = documentData;
-			if(text != null){
-				log('Looking for wikipedia articles');
-				text = text.replace(/[^a-zA-Z ]/g, '');
-				text = text.split(" ");
-				var articles = [];
-				for(var i = 0; i < text.length; i++){
-					if(text[i].charAt(0) === text[i].charAt(0).toLowerCase()){
-						continue;
-					}
-					var result = famousThing(text[i]);
-					if(result.length != 0){
-						articles.push(result);
-					}
-				}
-				articles = uniqueOnly(articles);
-				var articleOptions = [];
-				for(var i = 0; i < articles.length; i++){
-					articleOptions.push({
-						"name":articles[i],
-						"text":articles[i],
-						"data":{"type":"article","article":articles[i]},
-						"callback":wikipedia
-					});
-				}
-				addClippyOptions(articleOptions);
-			} else {
-				log('Text was null');
+		log('Looking for wikipedia articles');
+		text = text.replace(/[^a-zA-Z ]/g, '');
+		text = text.split(" ");
+		var articles = [];
+		for(var i = 0; i < text.length; i++){
+			if(text[i].charAt(0) === text[i].charAt(0).toLowerCase()){
+				continue;
 			}
-		} else {
-			addClippyOptions([]);
+			var result = famousThing(text[i]);
+			if(result.length != 0){
+				articles.push(result);
+			}
 		}
+		articles = uniqueOnly(articles);
+		var articleOptions = [];
+		for(var i = 0; i < articles.length; i++){
+			articleOptions.push({
+				"name":articles[i],
+				"text":articles[i],
+				"data":{"type":"article","article":articles[i]},
+				"callback":wikipedia
+			});
+		}
+		addClippyOptions(articleOptions);
 		agent.stop();
 		this.clippyInAction = false;
-	} else if(data["type"] == "article"){
+	} else {
 		log('Finding data on: ' + data["article"]);
 		getWikipediaArticle(data["article"],agent, displayWikiData);
-	}
+	} 
 }
 
 function uniqueOnly(list){
